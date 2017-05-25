3ab2c12d3852ae4cb381935a25fa4bcb5f60034d
MNArifin <arifinmumtazz@gmail.com>
2017-05-25 19:33:22 +0700

    Detail

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl118/bikmovies/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl118/bikmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl118.bikmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -8,15 +9,26 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
+import id.sch.smktelkom_mlg.privateassignment.xirpl118.bikmovies.adapter.NowPlayingAdapter;
+import id.sch.smktelkom_mlg.privateassignment.xirpl118.bikmovies.adapter.TopRatedAdapter;
 import id.sch.smktelkom_mlg.privateassignment.xirpl118.bikmovies.fragment.FavFragment;
 import id.sch.smktelkom_mlg.privateassignment.xirpl118.bikmovies.fragment.NowPlayingFragment;
 import id.sch.smktelkom_mlg.privateassignment.xirpl118.bikmovies.fragment.TopRatedFragment;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, NowPlayingAdapter.INowPlayingAdapter, TopRatedAdapter.ITopRatedAdapter
 {
+
+    public static final String RESULTTITLE = "resultTitle";
+    public static final String RESULTOVERVIEW = "resultOverview";
+    public static final String RESULTBACKDROP = "resultBackdrop";
+    public static final String RESULTPOSTER = "resultPoster";
+    public static final String RESULTRELEASE = "resultRelease";
+    public static final String RESULTAVERAGE = "resultAverage";
+    public static final String RESULTCOUNT = "resultCount";
+    public static final String RESULTPOPULARITY = "resultPopuarity";
+    public static final String RESULTLANGUAGE = "resultLanguage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,16 +37,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View view)
-//            {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-//            }
-//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -62,30 +64,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings)
-//        {
-//            return true;
-//        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -124,4 +102,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
     }
+
+    @Override
+    public void showNowPlaying(String poster_path, String title, String overview, String vote_average, String release_date, String backdrop_path, String vote_count, String popularity, String original_language)
+    {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(RESULTTITLE, title);
+        intent.putExtra(RESULTOVERVIEW, overview);
+        intent.putExtra(RESULTBACKDROP, backdrop_path);
+        intent.putExtra(RESULTPOSTER, poster_path);
+        intent.putExtra(RESULTRELEASE, release_date);
+        intent.putExtra(RESULTAVERAGE, vote_average);
+        intent.putExtra(RESULTCOUNT, vote_count);
+        intent.putExtra(RESULTPOPULARITY, popularity);
+        intent.putExtra(RESULTLANGUAGE, original_language);
+
+        startActivity(intent);
+    }
+
+    @Override
+    public void showTopRated(String poster_path, String title, String overview, String vote_average, String release_date, String backdrop_path, String vote_count, String popularity, String original_language)
+    {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(RESULTTITLE, title);
+        intent.putExtra(RESULTOVERVIEW, overview);
+        intent.putExtra(RESULTBACKDROP, backdrop_path);
+        intent.putExtra(RESULTPOSTER, poster_path);
+        intent.putExtra(RESULTRELEASE, release_date);
+        intent.putExtra(RESULTAVERAGE, vote_average);
+        intent.putExtra(RESULTCOUNT, vote_count);
+        intent.putExtra(RESULTPOPULARITY, popularity);
+        intent.putExtra(RESULTLANGUAGE, original_language);
+
+        startActivity(intent);
+    }
+
 }
